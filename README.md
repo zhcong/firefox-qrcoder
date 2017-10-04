6695dbd2054bdb5897c5f9af2e33b7dc6492cdcc
俺是一棵大葱 <1011.0011@163.com>
2017-10-04 18:08:24 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# 二维码助手
+二维码助手是一款支持firefox57+的插件，可以将正在浏览的网页上的链接文字以及网页地址转换成二维码，并显示在正在浏览的网页上。</br>
+![image](https://raw.githubusercontent.com/zhcong/firefox-qrcoder/master/screenshot.png)
+## Thank for：</br>
+[https://github.com/xstude/qrcode](https://github.com/xstude/qrcode)
+## another
+这是该插件在chrome下的项目
+https://github.com/zhcong/chrome-qrcoder
---
+++ eventPage.js
@@ -0,0 +1,31 @@
+﻿function check(table_id,data,tab){
+	if(tab.url.indexOf("about:") == 0){
+		chrome.pageAction.hide(table_id);
+	}else{
+		chrome.pageAction.show(table_id);
+	}
+};
+function draw(str,tab){
+	chrome.tabs.executeScript(null,{code: "var qr_text=\'"+str+"\'"});
+	console.log(str);
+	chrome.tabs.insertCSS(null,{file:'qrcoder_css.css'});
+	chrome.tabs.executeScript(null,{file: "codePanel.js"});
+}
+
+chrome.tabs.onUpdated.addListener(check);
+
+chrome.contextMenus.create({'title':'生成页面地址的二维码','contexts':['page'],'onclick':function(info,tab){
+	draw(info.pageUrl,tab);
+}});
+chrome.contextMenus.create({'title':'生成选中链接的二维码','contexts':['link'],'onclick':function(info,tab){
+	draw(info.linkUrl,tab);
+}});
+chrome.contextMenus.create({'title':'生成选中文字的二维码','contexts':['selection'],'onclick':function(info,tab){
+	draw(info.selectionText,tab);
+}});
+chrome.contextMenus.create({'title':'生成图片地址的二维码','contexts':['image'],'onclick':function(info,tab){
+	draw(info.srcUrl,tab);
+}});
+chrome.pageAction.onClicked.addListener(function(tab){
+	draw(tab.url,tab);
+});
